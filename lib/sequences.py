0e4097e8317c996b084c596644fb2746841770ae
Abigail Chelanagt <abigailchelah@gmail.com>
2025-05-08 13:15:27 +0300

    Add my files

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print("[]")
+        return
+    
+    a, b = 0, 1
+    fibonacci_sequence = []
+
+    for _ in range(length):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+
+    print(f"{fibonacci_sequence}")
